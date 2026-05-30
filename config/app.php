06047453c74980c0799e1040fa36393787956362
Dinesh Kumar <mapmydestination@gmail.com>
2026-05-30 20:10:31 +0530

    update

--- config/app.php
+++ config/app.php
@@ -144,10 +144,10 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
+        // App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        // App\Providers\EventServiceProvider::class,
+        // App\Providers\RouteServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
     ],
 
