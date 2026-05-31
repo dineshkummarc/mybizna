4cd4eaa34f6c7f919bae7999f682c49673b21d07
Dinesh Kumar <mapmydestination@gmail.com>
2026-05-31 11:17:04 +0530

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+	use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -18,6 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+		'username',
+        'is_admin',
+        'phone',
     ];
 
     /**
